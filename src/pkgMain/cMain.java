bbff2c98566395bdfc64004862b7c65496b074ec
Jon-Jon <Jon-Jon@128.4.86.94>
2017-02-08 18:46:56 -0500

    Updated rectangle and added name to main.

--- src/pkgMain/cMain.java
+++ src/pkgMain/cMain.java
@@ -3,7 +3,9 @@ package pkgMain;
 public class cMain {
 
 	public static void main(String[] args) {
-
+		/*
+		 * Github Account Name : jonjon007
+		 */
 		Rectangle rec = new Rectangle(1.5, 2.5);		
 		System.out.println("Rectangle Area: " + rec.Area());
 	}
